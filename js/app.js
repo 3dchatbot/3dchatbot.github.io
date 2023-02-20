432634a3082f14812dd48df11247b85a416d6fd2
ju3tin <ju3tin@hotmail.co.uk>
2023-02-20 06:10:20 +0000

    sdf

--- js/app.js
+++ js/app.js
@@ -156,7 +156,7 @@ function createDownloadLink(blob) {
 		  };
 		  var fd=new FormData();
 		  fd.append("audio_data",blob, filename);
-		  xhr.open("POST","upload.php",true);
+		  xhr.open("POST","https://pythonstt.onrender.com/read4",true);
 		  xhr.send(fd);
 	})
 	li.appendChild(document.createTextNode (" "))//add a space in between
